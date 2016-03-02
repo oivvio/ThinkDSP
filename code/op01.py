8dee3fbad503aa906706dcb3063d97532360177d
Oivvio Polite <oivvio@liberationtech.net>
2016-03-02 18:50:22 +0100

    tat

--- code/op01.py
+++ code/op01.py
@@ -1,7 +1,24 @@
 import thinkdsp
+import thinkplot
+import random
+from IPython.display import display
 
-cos_sig = thinkdsp.CosSignal(freq=440, amp=1.0, offset=0)
+signals = []
+start = 100
+stop = 2000
+n = 10
+for i in range(n):
+    freq = random.randint(100, 1000)
+    signals.append(thinkdsp.SinSignal(freq=freq, amp=1.0, offset=0))
+sig = sum(signals)
 
-wave = cos_sig.make_wave(duration=1, start=0, framerate=11025)
+print(sig.period, 1/sig.period)
 
-wave.plot()
+wave = sig.make_wave(duration=0.1/sig.period, start=0, framerate=22050)
+
+audio = wave.make_audio()
+display(audio)
+
+# spectrum = wave.make_spectrum()
+# spectrum.plot(high=1100)
+thinkplot.show()
