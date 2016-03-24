ddc7f2ea05f901d9a3ce2cd0601b4aac47ee8b8a
Oivvio Polite <oivvio@liberationtech.net>
2016-03-24 22:20:03 +0100

    tat

--- code/op01.py
+++ code/op01.py
@@ -1,7 +1,20 @@
 import thinkdsp
+import thinkplot
 
-cos_sig = thinkdsp.CosSignal(freq=440, amp=1.0, offset=0)
+afile = "/tmp/temp.wav"
+# sig1 = thinkdsp.CosSignal(freq=440, amp=1.0, offset=0)
+# sig2 = thinkdsp.CosSignal(freq=100, amp=1.0, offset=0)
 
-wave = cos_sig.make_wave(duration=1, start=0, framerate=11025)
 
-wave.plot()
+# sig = sig1 + sig2
+# wave = sig.make_wave(duration=1, start=0, framerate=11025)
+
+sig = thinkdsp.TriangleSignal(freq=1100, amp=1, offset=0)
+#sig = thinkdsp.SinSignal(freq=1100, amp=1, offset=0)
+
+wave = sig.make_wave(duration=1, framerate=10000)
+#wave.plot()
+wave.play(afile)
+spectrum = wave.make_spectrum()
+spectrum.plot()
+thinkplot.show()
